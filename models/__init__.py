a1b17b5167461ce513b7607137a098ea0ea46e78
mruc700 <tower5000reyes@gmail.com>
2025-10-07 00:30:32 -0400

    Delete models directory

--- models/__init__.py
+++
@@ -1,2 +0,0 @@
-# models/__init__.py
-# This file makes the models directory a Python package.
--- models/database.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def init_db(app):
-    """Inicializa la base de datos con la aplicación Flask."""
-    with app.app_context():
-        db.create_all()
